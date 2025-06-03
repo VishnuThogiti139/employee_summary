bd1bbf13cc9b484871f6ddaa5ea8c01a8b4fbb1d
VishnuThogiti139 <thogitivishnu1079@gmail.com>
2025-06-02 20:20:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 DB_CONFIG = {
     "host": st.secrets["MYSQL_HOST"],
-    "port": int(st.secrets["MYSQL_USER"]),
-    "user": st.secrets["DB_USER"],
+    "port": int(st.secrets["MYSQL_PORT"]),        # ✅ use correct key for port
+    "user": st.secrets["MYSQL_USER"],
     "password": st.secrets["MYSQL_PASSWORD"],
     "database": st.secrets["MYSQL_DB"]
 }
