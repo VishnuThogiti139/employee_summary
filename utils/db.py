bcf80faa68a5ca2121aa6a3b3a0ea8342c3e1cf1
VishnuThogiti139 <thogitivishnu1079@gmail.com>
2025-06-03 23:07:10 -0400

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,10 +1,5 @@
-from config import DB_CONFIG
 import mysql.connector
+from config import DB_CONFIG
 
-def get_db_connection():
-    try:
-        return mysql.connector.connect(**DB_CONFIG)
-    except mysql.connector.Error as err:
-        import streamlit as st
-        st.error(f"MySQL connection failed: {err}")
-        raise
+def get_connection():
+    return mysql.connector.connect(**DB_CONFIG)
