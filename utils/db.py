8998f1baad86d27983cc641ca70e6b8bba926e77
VISHNU THOGITI <thogitivishnu1079@gmail.com>
2025-07-15 21:35:42 -0400

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,5 +1,21 @@
 import mysql.connector
 from config import DB_CONFIG
+from contextlib import contextmanager
 
+@contextmanager
 def get_connection():
-    return mysql.connector.connect(**DB_CONFIG)
+    # """
+    # A context manager for handling MySQL database connections.
+    # """
+    conn = None
+    try:
+        # Establishing the connection
+        conn = mysql.connector.connect(**DB_CONFIG)
+        yield conn  # Yield the connection to the calling code
+    except mysql.connector.Error as err:
+        # Handling any database connection errors
+        raise Exception(f"Error connecting to the database: {err}")
+    finally:
+        # Close the connection when the block is exited, if it's valid
+        if conn and conn.is_connected():
+            conn.close()
